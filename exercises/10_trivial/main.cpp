30f5129a75e6f8cefc054c459e30e2005067c298
XJH <nil541203@gmail.com>
2026-01-05 11:22:49 +0800

    替换之前已完成内容

--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -9,9 +9,12 @@ struct FibonacciCache {
 
 // TODO: 实现正确的缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+	cache.cache[0] = 0;
+	cache.cache[1] = 1;
+    for (cache.cached = 2; cache.cached <= i; ++cache.cached) {
+        cache.cache[cache.cached] = cache.cache[cache.cached - 1] + cache.cache[cache.cached - 2];
     }
+    // 1 1 2 3 5 8 13 21 34 55
     return cache.cache[i];
 }
 
